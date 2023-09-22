6efe43abf15857f893a7337d7783db7ba64545f8
jdlms <97018563+jdlms@users.noreply.github.com>
2023-09-22 22:01:40 +0200

    repl initialized

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// return map[string]cliCommand{
+//     "help": {
+//         name:        "help",
+//         description: "Displays a help message",
+//         callback:    commandHelp,
+//     },
+//     "exit": {
+//         name:        "exit",
+//         description: "Exit the Pokedex",
+//         callback:    commandExit,
+//     },
+// }
 
 func main() {
-	fmt.Println("Hello!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+
+		scanned := scanner.Scan()
+		if !scanned {
+			break
+		}
+
+		line := scanner.Text()
+		if line == "quit" || line == "stop" {
+			fmt.Println("bye!")
+			break
+		}
+
+	}
+
 }
